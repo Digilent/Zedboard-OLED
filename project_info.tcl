0bbb635268f7d92f7eb370995d85a2083bcd0b8e
robert_bocos <robert-emilian.bocos@digilent.ro>
2020-11-09 13:46:06 +0200

    Upgraded project to v2020.1

--- project_info.tcl
+++ project_info.tcl
@@ -1,9 +1,21 @@
 # This is an automatically generated file used by digilent_vivado_checkout.tcl to set project options
-proc set_digilent_project_properties {proj_name} {
+proc set_project_properties_post_create_project {proj_name} {
     set project_obj [get_projects $proj_name]
-	set_property "part" "xc7z020clg484-1" $project_obj
-	set_property "board_part" "digilentinc.com:zedboard:part0:1.0" $project_obj
-	set_property "default_lib" "xil_defaultlib" $project_obj
-	set_property "simulator_language" "Mixed" $project_obj
-	set_property "target_language" "Verilog" $project_obj
+    set_property "part" "xc7z020clg484-1" $project_obj
+    set_property "board_part" "em.avnet.com:zed:part0:1.4" $project_obj
+    set_property "default_lib" "xil_defaultlib" $project_obj
+    set_property "simulator_language" "Mixed" $project_obj
+    set_property "target_language" "Verilog" $project_obj
+}
+
+proc set_project_properties_pre_add_repo {proj_name} {
+    set project_obj [get_projects $proj_name]
+    # default nothing
+}
+
+proc set_project_properties_post_create_runs {proj_name} {
+    set project_obj [get_projects $proj_name]
+	#Custom directives for synthesis and implementation
+	set_property STEPS.WRITE_BITSTREAM.ARGS.READBACK_FILE 0 [get_runs impl_1]
+	set_property STEPS.WRITE_BITSTREAM.ARGS.VERBOSE 0 [get_runs impl_1]
 }
