31e13545e160b3cabf85b75fdd2bee371876f4ea
robert_bocos <robert-emilian.bocos@digilent.ro>
2020-11-10 11:41:29 +0200

    Fixed coefficient file errors by changing board_part.

--- project_info.tcl
+++ project_info.tcl
@@ -2,7 +2,7 @@
 proc set_project_properties_post_create_project {proj_name} {
     set project_obj [get_projects $proj_name]
     set_property "part" "xc7z020clg484-1" $project_obj
-    set_property "board_part" "em.avnet.com:zed:part0:1.4" $project_obj
+    set_property "board_part" "digilentinc.com:zedboard:part0:1.0" $project_obj
     set_property "default_lib" "xil_defaultlib" $project_obj
     set_property "simulator_language" "Mixed" $project_obj
     set_property "target_language" "Verilog" $project_obj
