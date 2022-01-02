400507ba14ad3d7472dd6a8bf2a6ef14acc5aec4
Robin Mueller <robin.mueller.m@gmail.com>
2022-01-02 20:21:48 +0100

    removed path set command

--- project_info.tcl
+++ project_info.tcl
@@ -2,7 +2,6 @@
 proc set_digilent_project_properties {proj_name} {
     set project_obj [get_projects $proj_name]
 	set_property "part" "xc7z020clg484-1" $project_obj
-	set_property BOARD_PART_REPO_PATHS "/home/rmueller/.Xilinx/Vivado/2021.2/xhub/board_store/xilinx_board_store" $project_obj
 	set_property "board_part" "avnet.com:zedboard:part0:1.4" $project_obj
  	set_property "default_lib" "xil_defaultlib" $project_obj
 	set_property "simulator_language" "Mixed" $project_obj
