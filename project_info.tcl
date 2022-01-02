f2f724617d8e684ddd60050e422ebc261e670ee7
Robin Mueller <robin.mueller.m@gmail.com>
2022-01-02 20:20:42 +0100

    v2021.2 adaptions

--- project_info.tcl
+++ project_info.tcl
@@ -2,8 +2,9 @@
 proc set_digilent_project_properties {proj_name} {
     set project_obj [get_projects $proj_name]
 	set_property "part" "xc7z020clg484-1" $project_obj
-	set_property "board_part" "digilentinc.com:zedboard:part0:1.0" $project_obj
-	set_property "default_lib" "xil_defaultlib" $project_obj
+	set_property BOARD_PART_REPO_PATHS "/home/rmueller/.Xilinx/Vivado/2021.2/xhub/board_store/xilinx_board_store" $project_obj
+	set_property "board_part" "avnet.com:zedboard:part0:1.4" $project_obj
+ 	set_property "default_lib" "xil_defaultlib" $project_obj
 	set_property "simulator_language" "Mixed" $project_obj
 	set_property "target_language" "Verilog" $project_obj
 }
